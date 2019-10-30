67da4fe5d7a9fdef69dd1ec49af0a4295f353129
Luis M. Gallardo D <lgallard@gmail.com>
2019-10-30 17:59:04 -0300

    Add admin_create_user_config block

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "aws_cognito_user_pool" "pool" {
+  name = var.user_pool_name
+
+
+  email_verification_message = var.email_verification_message
+
+  # admin_create_user_config
+  dynamic "admin_create_user_config" {
+    for_each = local.admin_create_user_config
+    content {
+      allow_admin_create_user_only = lookup(admin_create_user_config.value, "allow_admin_create_user_only")
+      unused_account_validity_days = lookup(admin_create_user_config.value, "unused_account_validity_days")
+
+      dynamic "invite_message_template" {
+        for_each = lookup(admin_create_user_config.value, "email_message", null) == null && lookup(admin_create_user_config.value, "email_subject", null) == null && lookup(admin_create_user_config.value, "sms_message", null) == null ? [] : [1]
+        content {
+          email_message = lookup(admin_create_user_config.value, "email_message")
+          email_subject = lookup(admin_create_user_config.value, "email_subject")
+          sms_message   = lookup(admin_create_user_config.value, "sms_message")
+        }
+      }
+    }
+  }
+}
+
+locals {
+  # admin_create_user_config
+  # If no admin_create_user_config list is provided, build a admin_create_user_config using the default values
+  admin_create_user_config_default = {
+    allow_admin_create_user_only = lookup(var.admin_create_user_config, "allow_admin_create_user_only", null) == null ? var.admin_create_user_config_allow_admin_create_user_only : lookup(var.admin_create_user_config, "allow_admin_create_user_only")
+    unused_account_validity_days = lookup(var.admin_create_user_config, "unused_account_validity_days", null) == null ? var.admin_create_user_config_unused_account_validity_days : lookup(var.admin_create_user_config, "unused_account_validity_days")
+    email_message                = lookup(var.admin_create_user_config, "email_message", null) == null ? var.admin_create_user_config_email_message : lookup(var.admin_create_user_config, "email_message")
+    email_subject                = lookup(var.admin_create_user_config, "email_subject", null) == null ? var.admin_create_user_config_email_subject : lookup(var.admin_create_user_config, "email_subject")
+    sms_message                  = lookup(var.admin_create_user_config, "sms_message", null) == null ? var.admin_create_user_config_sms_message : lookup(var.admin_create_user_config, "sms_message")
+
+  }
+
+  admin_create_user_config = [local.admin_create_user_config_default]
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "user_pool_name" {
+  description = "The name of the user pool"
+  type        = string
+}
+
+variable "email_verification_message" {
+  description = "A string representing the email verification message. Conflicts with `verification_message_template` configuration block `email_message` argument"
+  type        = string
+  default     = null
+}
+
+# admin_create_user_config
+variable "admin_create_user_config" {
+  description = "The configuration for AdminCreateUser requests"
+  type        = map
+  default     = {}
+}
+
+variable "admin_create_user_config_allow_admin_create_user_only" {
+  description = "Set to True if only the administrator is allowed to create user profiles. Set to False if users can sign themselves up via an app"
+  type        = bool
+  default     = true
+}
+
+variable "admin_create_user_config_unused_account_validity_days" {
+  description = "The user account expiration limit, in days, after which the account is no longer usable"
+  type        = number
+  default     = 7
+}
+
+variable "admin_create_user_config_email_message" {
+  description = "The message template for email messages. Must contain {username} and {####} placeholders, for username and temporary password, respectively"
+  type        = string
+  default     = "{username}, your verification code is {####}."
+}
+
+
+variable "admin_create_user_config_email_subject" {
+  description = "The subject line for email messages"
+  type        = string
+  default     = "Your verification code"
+}
+
+variable "admin_create_user_config_sms_message" {
+  description = "- The message template for SMS messages. Must contain {username} and {####} placeholders, for username and temporary password, respectively"
+  type        = string
+  default     = "Your username is {username} and temporary password is {####}."
+}
